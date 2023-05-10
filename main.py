1e6ecab953144641a18a077c89fedeedb674bc16
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-05-10 09:41:59 +0200

    optimized  code

--- main.py
+++ main.py
@@ -27,21 +27,10 @@ while True:
     if not ret:
         break
 
-    # Resize the frame to a width of 1000 pixels
-    # frame = imutils.resize(frame, width=1000)
-
-    # Apply background subtraction
+    # Apply background subtraction, morphological operations, and find contours
     fgmask = bs.apply(frame)
-
-    # Apply morphological operations to remove noise
-    fgmask = cv2.erode(fgmask, None, iterations=2)
-    fgmask = cv2.dilate(fgmask, None, iterations=2)
-
-    # Find contours in the foreground mask
-    contours = cv2.findContours(
-        fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-    contours = imutils.grab_contours(contours)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, None)
+    contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Loop over the contours
     for contour in contours:
@@ -56,7 +45,7 @@ while True:
         # If recording is not in progress, start recording
         if not recording:
             # Initialize the first frame and set the recording flag
-            first_frame = frame.copy()
+            first_frame = frame
             recording = True
 
             # Create a video writer object
@@ -79,12 +68,12 @@ while True:
     # If no motion is detected and recording is in progress, start the no motion timer
     if not contours and recording:
         out.write(frame)
+
         # Check if the no motion timeout has been reached
         if time.time() - last_motion_time >= NO_MOTION_TIMEOUT:
-            if not contours:
-                out.release()
-                recording = False
-                print("Recording stopped")
+            out.release()
+            recording = False
+            print("Recording stopped")
 
     # Display the resulting frame
     cv2.imshow("frame", frame)

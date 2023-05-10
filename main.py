9e7ce6723ff085e6419d79cdf5de2ce86ea5a844
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-05-10 09:39:49 +0200

    fixed `NO_MOTION_TIMEOUT`

--- main.py
+++ main.py
@@ -1,47 +1,100 @@
+import imutils
 import cv2
-import os
+import datetime
+import time
 
-# RTSP URL
-RTSP_URL = "rtsp://192.168.1.254/live"
-
-# Create a VideoCapture object for the RTSP stream
-# cap = cv2.VideoCapture(RTSP_URL)
+url = ""
 cap = cv2.VideoCapture(0)
 
-# Check if the VideoCapture object was successfully opened
-if not cap.isOpened():
-    print("Failed to open RTSP stream.")
-    exit(-1)
+# Create a background subtractor object
+bs = cv2.createBackgroundSubtractorMOG2()
 
-# Set a lower frame rate
-cap.set(cv2.CAP_PROP_FPS, 5)
+# Initialize the first frame and a flag to indicate recording
+first_frame = None
+recording = False
 
-# Counter to skip frames
-frame_count = 0
+# Initialize the video writer
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = None
+
+# Initialize a timer for no motion detection
+last_motion_time = time.time()
+NO_MOTION_TIMEOUT = 3
 
 while True:
-    # Read a frame from the video stream
     ret, frame = cap.read()
 
-    # If no frame was read, break out of the loop
     if not ret:
         break
 
-    # Skip every other frame
-    if frame_count % 2 == 0:
-        frame_count += 1
-        continue
+    # Resize the frame to a width of 1000 pixels
+    # frame = imutils.resize(frame, width=1000)
 
-    # Display the current frame
-    cv2.imshow("Frame", frame)
+    # Apply background subtraction
+    fgmask = bs.apply(frame)
 
-    # Wait for a key press to exit
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
+    # Apply morphological operations to remove noise
+    fgmask = cv2.erode(fgmask, None, iterations=2)
+    fgmask = cv2.dilate(fgmask, None, iterations=2)
+
+    # Find contours in the foreground mask
+    contours = cv2.findContours(
+        fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    contours = imutils.grab_contours(contours)
+
+    # Loop over the contours
+    for contour in contours:
+        # Ignore small contours
+        if cv2.contourArea(contour) < 500:
+            continue
+
+        # Draw a bounding box around the contour
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # If recording is not in progress, start recording
+        if not recording:
+            # Initialize the first frame and set the recording flag
+            first_frame = frame.copy()
+            recording = True
+
+            # Create a video writer object
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            out = cv2.VideoWriter(
+                "./videos/" + timestamp + ".mp4",
+                fourcc,
+                20.0,
+                (frame.shape[1], frame.shape[0]),
+                True,
+            )
+
+        # If recording is in progress, write the frame to the video file
+        else:
+            out.write(frame)
+
+            # Reset the no motion timer
+            last_motion_time = time.time()
+
+    # If no motion is detected and recording is in progress, start the no motion timer
+    if not contours and recording:
+        out.write(frame)
+        # Check if the no motion timeout has been reached
+        if time.time() - last_motion_time >= NO_MOTION_TIMEOUT:
+            if not contours:
+                out.release()
+                recording = False
+                print("Recording stopped")
+
+    # Display the resulting frame
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) == ord("q"):
         break
 
-    frame_count += 1
-
-# Clean up
+# Release the video capture and writer objects
 cap.release()
+if recording:
+    out.release()
+
 cv2.destroyAllWindows()
